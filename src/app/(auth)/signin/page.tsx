459fe1732146b238a7146963a5bbddbb0030df05
Firouzzadeh <mfirouzzadeh99@gmail.com>
2024-07-03 09:22:22 +0330

    Complete login-verify

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -3,39 +3,39 @@ import Link from "next/link";
 import logo from "../../../../public/images/android-chrome-192x192.png"
 export default async function SignIn() {
   return (
-    <div className="px-10 py-4 w-[330px] h-[400px] md:w-[450px] md:h-[450px] bg-base-50 flex justify-around items-center flex-col rounded-sm">
+    <div className="px-10 py-4 w-[330px] h-[360px] bg-base-50 flex justify-around items-center flex-col rounded-md">
       <div className="flex gap-3 mt-6">
         <Image
           src={logo}
           width={0}
           style={{objectFit: "contain"}}
           height={0}
-          className="w-14 h-14 md:w-20 md:h-20 mt-3 md:mt-0"
+          className="w-14 h-14 mt-3  "
           alt="logo"
         />
         <div className="flex mt-2 gap-2 flex-col">
-          <p className="text-base-content-slider font-bold">ورود / ثبت نام</p>
-          <p className="text-[10px] md:text-sm mb-0 md:mb-0">
+          <p className="text-base-content-slider font-bold text-[12px]">ورود / ثبت نام</p>
+          <p className="text-[11px] mb-0">
             جهت ورود یا ثبت‌نام در آن، شماره موبایل خود را وارد کنید.
           </p>
         </div>
       </div>
       <div className="flex flex-col gap-4">
         <div className="grid grid-cols-12 gap-2">
-          <input className="col-span-10 p-2 rounded-md" />
-          <input value={"98+"} className="col-span-2 p-2 rounded-md" />
+          <input className="col-span-10 p-1.5 rounded-md" />
+          <input value={"98+"} className="col-span-2 p-2 text-[12px] rounded-md" />
         </div>
         <Link
-          className="w-auto text-xs md:text-sm p-2.5 bg-gradient-to-r from-gradient-second to-gradient-first font-bold text-white items-center flex text-center justify-center rounded-[7px] sm:w-auto"
+          className="text-xs  p-2 bg-gradient-to-r from-gradient-second to-gradient-first font-bold text-white items-center flex text-center justify-center rounded-[7px] w-auto"
           href={"/"}
         >
           تایید کد
         </Link>
       </div>
       <div className="mr-auto flex justify-center items-center gap-1 cursor-pointer">
-        <Link href={"/"}>بازگشت</Link>
+        <Link href={"/"} className="text-sm">بازگشت</Link>
         <span>
-          <Image src={"/images/icon.webp"} alt="arrow" width={6} height={6} />
+          <Image src={"/images/icon.webp"} style={{objectFit: "contain"}} className="ml-3" alt="arrow" width={6} height={6} />
         </span>
       </div>
     </div>
--- src/app/(auth)/verify/page.tsx
+++ src/app/(auth)/verify/page.tsx
@@ -4,19 +4,19 @@ import logo from "../../../../public/images/android-chrome-192x192.png"
 
 export default function Verify() {
   return (
-    <div className="px-10 py-4 w-[330px] h-[400px] md:w-[450px] md:h-[450px] bg-base-50 flex justify-around items-center flex-col rounded-sm">
+    <div className="px-10 py-4 w-[330px] h-[390px] bg-base-50 flex justify-around items-center flex-col rounded-md">
       <div className="flex gap-3 mt-10">
         <Image
           src={logo}
           width={0}
           objectFit="contain"
           height={0}
-          className="w-14 h-14 md:w-20 md:h-20 mt-3 md:mt-0"
+          className="w-14 h-14 mt-3"
           alt="logo"
         />
         <div className="flex mt-2 flex-col gap-2">
-          <p className="text-base-content-slider font-bold">ورود به آن</p>
-          <p className="text-[10px] md:text-sm mb-6 md:mb-0">
+          <p className="text-base-content-slider font-bold text-[12px]">ورود به آن</p>
+          <p className="text-[11px] mb-6">
             جهت ورود یا ثبت‌نام در آن، شماره موبایل خود را وارد کنید.
           </p>
         </div>
@@ -31,17 +31,17 @@ export default function Verify() {
         </div>
         <p className="text-center font-bold text-lg">22 : 01</p>
         <Link
-          className="w-auto text-xs md:text-sm p-2.5 bg-gradient-to-r from-gradient-second to-gradient-first font-bold text-white items-center flex text-center justify-center rounded-[7px] sm:w-auto"
+          className="w-auto text-xs md:text-sm p-2 bg-gradient-to-r from-gradient-second to-gradient-first font-bold text-white items-center flex text-center justify-center rounded-[7px] sm:w-auto"
           href={"/"}
         >
           تایید کد
         </Link>
-        <p className="text-center cursor-pointer"> ارسال دوباره کد</p>
+        <p className="text-center text-[12px] cursor-pointer"> ارسال دوباره کد</p>
       </div>
       <div className="mr-auto flex justify-center items-center gap-1 cursor-pointer">
-        <Link href={"/signin?html"}>بازگشت</Link>
+        <Link href={"/signin?html"} className="text-sm">بازگشت</Link>
         <span>
-          <Image src={"/images/icon.webp"} alt="arrow" width={6} height={6} />
+          <Image src={"/images/icon.webp"} style={{objectFit:"contain"}} className="ml-3" alt="arrow" width={6} height={6} />
         </span>
       </div>
     </div>
