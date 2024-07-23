d142b4ed5e6debaba76ae13ecbba12facdee9869
Sayied Amir Ahmadi (Arch PC) <s.amir.ahmadi.n@gmail.com>
2024-07-23 09:29:34 +0330

    change layout size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -37,6 +37,13 @@ const config: Config = {
     container: {
       center: true,
     },
+    screens: {
+      sm: '600px',
+      md: '728px',
+      lg: '984px',
+      xl: '1240px',
+      '2xl': '1720px',
+    },
   },
   darkMode: "class",
   plugins: [],
