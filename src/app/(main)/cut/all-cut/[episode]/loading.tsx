9ab0f933d0de9bcd8c7c276f1e6cf1dad1a6fc40
Firouzzadeh <mfirouzzadeh99@gmail.com>
2024-09-16 22:55:31 +0330

    fix: _#317 loading

--- src/app/(main)/cut/all-cut/[episode]/loading.tsx
+++ src/app/(main)/cut/all-cut/[episode]/loading.tsx
@@ -5,15 +5,9 @@ const loading = () => {
     <div className="w-full container h-screen bg-gray-200 p-4">
       <div className="grid grid-cols-8 gap-4 h-full mt-3">
         {[...Array(24)].map((_, index) => (
-          <div>
-            <div
-              key={index}
-              className="bg-base-50 h-[210px] animate-pulse rounded-lg"
-            ></div>
-            <div
-              key={index}
-              className="bg-base-50 w-20 h-3 mt-1 animate-pulse rounded-lg"
-            ></div>
+          <div key={index}>
+            <div className="bg-base-50 h-[210px] animate-pulse rounded-lg"></div>
+            <div className="bg-base-50 w-20 h-3 mt-1 animate-pulse rounded-lg"></div>
           </div>
         ))}
       </div>
